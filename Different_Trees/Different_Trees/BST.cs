3eed555099d36d957388051f119270a834a8d5b8
Nick Zhao <nickzrz@hotmail.com>
2018-06-03 14:31:44 -0700

    Initial commit

---
+++ Different_Trees/Different_Trees/BST.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Different_Trees
+{
+    class BST<T> where T : IComparable<T>
+    {
+        public T Node;
+
+        void Search(T key)
+        {
+
+        }
+
+        void Insert(T value)
+        {
+
+        }
+
+        void Delete(T value)
+        {
+
+        }
+
+        bool IsEmpty()
+        {
+            throw new Exception(NotImplementedException\)
+        }
+    }
+}
