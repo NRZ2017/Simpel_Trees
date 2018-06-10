d7393d281ca0f9c940ff0a3a00209f74113051e9
Nick Zhao <nickzrz@hotmail.com>
2018-06-10 14:30:49 -0700

    Finished search function, need to finish inserting

--- Different_Trees/Different_Trees/BST.cs
+++ Different_Trees/Different_Trees/BST.cs
@@ -8,26 +8,34 @@ namespace Different_Trees
 {
     class BST<T> where T : IComparable<T>
     {
-        public T Node;
+        BSTNodes<T> Root;
 
         void Search(T key)
         {
+            BSTNodes<T> current = Root;
+            while (current != null)
+            {
 
+            }
+            throw new NotImplementedException();
         }
 
         void Insert(T value)
         {
 
+            throw new NotImplementedException();
         }
 
         void Delete(T value)
         {
 
+            throw new NotImplementedException();
         }
 
         bool IsEmpty()
         {
-            throw new Exception(NotImplementedException\)
+
+            throw new NotImplementedException();
         }
     }
 }
